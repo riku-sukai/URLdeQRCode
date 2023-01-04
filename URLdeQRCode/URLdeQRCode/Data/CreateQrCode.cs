33c322a3d9344e6469a74de0ea7b7143f39262f0
sukai-riku <sukai.riku.work@gmail.com>
2023-01-04 20:57:47 +0900

    説明文とフォームを追加しました。QRコードを生成するクラスを実装するところから開始する。

---
+++ URLdeQRCode/URLdeQRCode/Data/CreateQrCode.cs
@@ -0,0 +1,13 @@
+﻿using System.Drawing.Imaging;
+using System.IO;
+using System;
+using ZXing.QrCode;
+using ZXing;
+using System.Drawing;
+
+namespace URLdeQRCode.Data
+{
+    public static class CreateQrCode
+    {
+    }
+}
